a544e34a4d86d554aca750613b2c014158d59063
Adarsh Venkataraman <Adarsh@wireless-10-146-212-201.public.utexas.edu>
2019-03-04 10:27:49 -0600

    Scraper works; gives quotes and authors

--- scraper.py
+++ scraper.py
@@ -1,6 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "quotes.toscrape.com"
-response = requests.get(url)
+url = "http://quotes.toscrape.com/"
+response = requests.get(url) #option to add timeout
 content = BeautifulSoup(response.content, "html.parser")
+
+try:
+    '''for quote in content.find_all('span', attrs={"class": "text"}):
+        print(quote.text,'\n') 
+    
+    for author in content.find_all('small', attrs={"class": "author"}):
+        print(author.text)'''
+
+    quotes = content.find_all('span', attrs = {"class": "text"})
+    authors = content.find_all('small', attrs = {"class": "author"})
+
+    for i in range(len(quotes)):
+        print(quotes[i].text)
+        print(authors[i].text)
+        print('\n')
+        
+    '''for quote in content.find_all('span'):
+        quote_content = quote.find(class_ = 'text').text
+        print(quote_content)'''
+
+    '''for quote in content.findAll(class_ = ['quote', 'text', 'author'])[1]('a'):
+    print(quote.text)'''
+
+except AttributeError:
+    print("Nonetype Attribute")
+
+except SyntaxError:
+    print("Incorrect Syntax")
+
+except:
+    print("Error happened")
