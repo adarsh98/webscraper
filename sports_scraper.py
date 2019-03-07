e1f3f59d4679f508f253f06622f4b00bddfe5ac9
Adarsh Venkataraman <Adarsh@wireless-10-147-98-18.public.utexas.edu>
2019-03-06 20:56:41 -0600

    Started trying stuff out with a sports API

--- sports_scraper.py
+++ sports_scraper.py
@@ -1,2 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import csv
+
+
+url = "http://soccer.sportsopendata.net/v1/leagues/premier-league/seasons/16-17/teams" 
+response = requests.get(url)
+content = BeautifulSoup(response.content, "html.parser")
+
+print(content) 
