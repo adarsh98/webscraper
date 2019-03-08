48c6aed916f0ba474683409b64a7630da8ac202d
Adarsh Venkataraman <Adarsh@Adarshs-MacBook-Pro.local>
2019-03-08 01:19:22 -0600

    Wrote more to json file

--- quote_scraper.py
+++ quote_scraper.py
@@ -1,24 +1,48 @@
 from bs4 import BeautifulSoup
+from scrapely import Scraper
 import requests
 import json
+
+'''quote_list = []
+
+def scrape_func(content):
+    try:
+        quotes = content.find_all('span', attrs = {"class": "text"})
+        authors = content.find_all('small', attrs = {"class": "author"})
+
+        for i in range(len(quotes)):
+            quote_obj = {"Quote": quotes[i].text, "Author": authors[i].text}
+            quote_list.append(quote_obj)
+
+    except AttributeError:
+        print("Nonetype Attribute")
+
+    except SyntaxError:
+        print("Incorrect Syntax")
+
+    except IndexError:
+        print("Index Out of Range")
+
+    except:
+        print("Error Happened")
+
+
+'''
 
 url = "http://quotes.toscrape.com/"
 response = requests.get(url) #option to add timeout
 content = BeautifulSoup(response.content, "html.parser")
 
+url1 = "http://quotes.toscrape.com/page/2/"
+response1 = requests.get(url1)
+content1 = BeautifulSoup(response1.content, "html.parser")
+
 try:
-    '''for quote in content.find_all('span', attrs={"class": "text"}):
-        print(quote.text,'\n') 
-    
-    for author in content.find_all('small', attrs={"class": "author"}):
-        print(author.text)'''
 
     quotes = content.find_all('span', attrs = {"class": "text"})
     authors = content.find_all('small', attrs = {"class": "author"})
-    
+
     quote_list = []
-
-
 
     for i in range(len(quotes)):
         quote_obj = {"Quote": quotes[i].text, "Author": authors[i].text}
@@ -27,18 +51,25 @@ try:
         print(authors[i].text)
         print('\n')'''
 
+
+    quotes = content1.find_all('span', attrs= {"class": "text"})
+    authors = content1.find_all('small', attrs= {"class": "author"})
+
+    for j in range(len(quotes)):
+        quote_obj = {"Quote": quotes[j].text, "Author": authors[j].text}
+        quote_list.append(quote_obj) 
+
     with open('quotedata.json', 'w') as outfile:
         json.dump(quote_list, outfile)
         
-    '''for quote in content.find_all('span'):
-        quote_content = quote.find(class_ = 'text').text
-        print(quote_content)'''
-
 except AttributeError:
     print("Nonetype Attribute")
 
 except SyntaxError:
     print("Incorrect Syntax")
 
+except IndexError:
+    print("Index Out of Range")
+
 except:
-    print("Error happened")
+    print("Error Happened")
