2ff577dc3bc31d35b8fc591393b3511d4637cc9a
Adarsh Venkataraman <Adarsh@Adarshs-MacBook-Pro.local>
2019-03-08 01:31:55 -0600

    Did the smart thing and wrote a function for the repetitive process

--- quote_scraper.py
+++ quote_scraper.py
@@ -3,7 +3,7 @@ from scrapely import Scraper
 import requests
 import json
 
-'''quote_list = []
+quote_list = []
 
 def scrape_func(content):
     try:
@@ -27,49 +27,25 @@ def scrape_func(content):
         print("Error Happened")
 
 
-'''
+def main():
 
-url = "http://quotes.toscrape.com/"
-response = requests.get(url) #option to add timeout
-content = BeautifulSoup(response.content, "html.parser")
+    url = "http://quotes.toscrape.com/"
+    response = requests.get(url) #option to add timeout
+    content = BeautifulSoup(response.content, "html.parser")
 
-url1 = "http://quotes.toscrape.com/page/2/"
-response1 = requests.get(url1)
-content1 = BeautifulSoup(response1.content, "html.parser")
+    url1 = "http://quotes.toscrape.com/page/2/"
+    response1 = requests.get(url1)
+    content1 = BeautifulSoup(response1.content, "html.parser")
 
-try:
+    url2 = "http://quotes.toscrape.com/page/3/"
+    response2 = requests.get(url2)
+    content2 = BeautifulSoup(response2.content, "html.parser")
 
-    quotes = content.find_all('span', attrs = {"class": "text"})
-    authors = content.find_all('small', attrs = {"class": "author"})
-
-    quote_list = []
-
-    for i in range(len(quotes)):
-        quote_obj = {"Quote": quotes[i].text, "Author": authors[i].text}
-        quote_list.append(quote_obj)
-        '''print(quotes[i].text)
-        print(authors[i].text)
-        print('\n')'''
-
-
-    quotes = content1.find_all('span', attrs= {"class": "text"})
-    authors = content1.find_all('small', attrs= {"class": "author"})
-
-    for j in range(len(quotes)):
-        quote_obj = {"Quote": quotes[j].text, "Author": authors[j].text}
-        quote_list.append(quote_obj) 
+    scrape_func(content)
+    scrape_func(content1)
+    scrape_func(content2)
 
     with open('quotedata.json', 'w') as outfile:
         json.dump(quote_list, outfile)
+main()
         
-except AttributeError:
-    print("Nonetype Attribute")
-
-except SyntaxError:
-    print("Incorrect Syntax")
-
-except IndexError:
-    print("Index Out of Range")
-
-except:
-    print("Error Happened")
